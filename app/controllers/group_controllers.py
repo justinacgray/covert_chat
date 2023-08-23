bc6e79807230a3281eb7d5098d14ec69db81a351
Justina <justinacgray@gmail.com>
2023-08-22 22:26:42 -0400

    got socketio setup testing CREATE group and members

--- app/controllers/group_controllers.py
+++ app/controllers/group_controllers.py
@@ -2,24 +2,44 @@ from app import APP
 from flask import render_template, redirect, request, session, flash
 from app import socketio
 from flask_socketio import SocketIO, emit
+from app.models import person_model, message_model, group_model
+# socketio = SocketIO()
 
-
+users = {}
 
 @APP.route("/group-chat")
 def render_group_chat(): 
-    return render_template('group_chat.html')
+    if 'user_id' not in session:
+        return redirect("/")
+    return render_template("dashboard.html", all_users = person_model.Person.get_all_users(), chat_list = message_model.Message.logged_in_user_active_chats(session['user_id']))
+
+@APP.route('/create-group-chat', methods= ['POST'])
+def create_group_and_members():
+    new_group_dict = {
+        "group_name" : request.form['group_name'],
+        "creator_user_id" : session['user_id'],
+        "group_members" : request.form.getlist("group_members")
+    }
+    
+    print("##### GROUP MEMBERS ######", new_group_dict)
+    
+    new_group = group_model.Group.create_group(new_group_dict)
+    
+    new_group_member = group_model.GroupMembers.create_members_per_group()
+    # todo - need to get a list of users from request.form
+    
+    return redirect('/dashboard')
 
 
-@socketio.on('group-msgs')
-def enter_group(data):
-    print('shalom' + data)
-    emit('joined_group', {'message': 'You have joined the group chat.'})
+@socketio.on('connect')
+def enter_group():
+    print('shalom friend')
+    
 
-
-@socketio.on('group-msgs')
-def enter_group(data):
-    print('shalom' + data)
-    emit('group-msgs', {'message': 'You have joined the group chat.'})
+@socketio.on('user_join')
+def user_enters(username):
+    print(f" Shalom {username}! ")
+    users[username] = request.sid
 
 
 
--- app/models/group_model.py
+++ app/models/group_model.py
@@ -1,23 +1,35 @@
-from app.config.mysqlconnection import connectToMySQL
+from app.config.mysqlconnection import MySQLConnection, connectToMySQL
+import random
+from flask import flash, session
+
 
 class Group:
     db = 'chat_db'
 
     def __init__(self, room_data):
-        self.room_id = room_data['room_id']
-        self.title = room_data['title'] 
-        self.creator_user_id = room_data['creator_user_id']
+        self.group_id = room_data['group_id']
+        self.group_name = room_data['group_name'] 
         self.created_at = room_data['created_at']
         self.updated_at = room_data['updated_at']
-        self.messages = []
+        self.creator_user_id = room_data['creator_user_id']
+        self.group_members = []
 
-    @classmethod
-    def hello(cls):
-        pass
     
     @classmethod
-    def create_group(cls):
-        pass
+    def create_group(cls, group_data):
+        if not cls.valid_group(group_data):
+            return False
+        
+        parsed_group_dict = cls.parse_group_data(group_data)
+        query = '''
+            INSERT INTO groups (group_id, group_name, creator_user_id )
+            VALUES ( %(group_id)s, %(group_name)s, %(creator_user_id)s);
+        '''
+        
+        new_group_id = MySQLConnection(cls.db).query_db(query, parsed_group_dict)
+        print("RESULTS group_data ====> ", new_group_id)
+        
+        return new_group_id
     
 
     @classmethod
@@ -32,9 +44,35 @@ class Group:
     def delete_message_in_group(cls):
         pass
     
-
+    
+    @staticmethod
+    def valid_group(group_dict):
+        is_valid =True
+        if len(group_dict["group_name"]) < 2:
+            flash( "Group Name must be at least 2 characters", category='error')
+            print("group name error")
+            is_valid = False
+        return is_valid
+    
+    @staticmethod
+    def parse_group_data(group_data):
+        parsed_data = {
+            # 'id': uuid4().hex,
+            'group_id' : random.getrandbits(22),
+            'group_name' : group_data['group_name'],
+            'creator_user_id' : group_data['creator_user_id']
+        }
+        print("$$$$$$$$$$$$$ parsed group data ===>" , parsed_data)
+        return parsed_data 
 
 class GroupMembers:
-    pass
-
-# should I include messages_id?
+    def __init__(self, group_members_data):
+        self.group_id = group_members_data['group_id']
+        self.group_name = group_members_data['group_name']
+        self.persons_user_id = group_members_data['persons_user_id']
+        self.created_at = group_members_data['created_at']
+        self.updated_at = group_members_data['updated_at']
+        
+    @classmethod
+    def create_members_per_group():
+        pass
